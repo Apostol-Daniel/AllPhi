ee62e67a322411b9a16872fb6cbec2fad5820e2f
Apostol Marian-Daniel <marian-daniel.apostol@student.howest.be>
2024-10-09 21:52:09 +0200

    Create Customer.cs and Order.cs

---
+++ AllPhi.Api/Models/Customer.cs
@@ -0,0 +1,10 @@
+namespace AllPhi.Api.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public List<Order>? Orders { get; set; }
+}
---
+++ AllPhi.Api/Models/Order.cs
@@ -0,0 +1,12 @@
+namespace AllPhi.Api.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public DateTime CreationDate { get; set; }
+    public bool IsCancelled { get; set; }
+    public int CustomerId { get; set; }
+    public Customer? Customer { get; set; } 
+}
