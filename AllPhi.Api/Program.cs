ea0d521588256f3d326126317832410b2b806ae5
Apostol Marian-Daniel <marian-daniel.apostol@student.howest.be>
2024-10-10 15:43:09 +0200

    Register services

--- AllPhi.Api/Program.cs
+++ AllPhi.Api/Program.cs
@@ -1,3 +1,6 @@
+using AllPhi.Api.Data;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AllPhiConnection")));
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
 var app = builder.Build();
 
