8573cc5c776f93ad87988801b7ab5c61d10b99d9
Apostol Marian-Daniel <marian-daniel.apostol@student.howest.be>
2024-10-10 22:36:38 +0200

    Make routing lowercase

--- AllPhi.Api/Program.cs
+++ AllPhi.Api/Program.cs
@@ -18,6 +18,8 @@ builder.Host.UseSerilog();
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
